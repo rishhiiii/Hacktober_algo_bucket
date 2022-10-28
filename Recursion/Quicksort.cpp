b38aa681fdce6e4e03c3e52c3bfa31573356196c
rishhiiii <87380598+rishhiiii@users.noreply.github.com>
2022-10-28 23:28:09 +0530

    Update Quicksort.cpp

--- Recursion/Quicksort.cpp
+++ Recursion/Quicksort.cpp
@@ -1,39 +1,56 @@
-#include <iostream>
-
+#include
 using namespace std;
 
-int partition(int a[], int start, int end)
-{
-    int pivot = a[end];
-    int i = (start - 1);
-
-    for (int j = start; j <= end - 1; j++)
-    {
-
-        if (a[j] < pivot)
-        {
-            i++;
-            int t = a[i];
-            a[i] = a[j];
-            a[j] = t;
-        }
-    }
-    int t = a[i + 1];
-    a[i + 1] = a[end];
-    a[end] = t;
-    return (i + 1);
+void swap(int arr[] , int pos1, int pos2){
+	int temp;
+	temp = arr[pos1];
+	arr[pos1] = arr[pos2];
+	arr[pos2] = temp;
 }
 
-void quick(int a[], int start, int end)
-{
-    if (start < end)
-    {
-        int p = partition(a, start, end);
-        quick(a, start, p - 1);
-        quick(a, p + 1, end);
-    }
+int partition(int arr[], int low, int high, int pivot){
+	int i = low;
+	int j = low;
+	while( i <= high){
+		if(arr[i] > pivot){
+			i++;
+		}
+		else{
+			swap(arr,i,j);
+			i++;
+			j++;
+		}
+	}
+	return j-1;
 }
 
+void quickSort(int arr[], int low, int high){
+	if(low < high){
+	int pivot = arr[high];
+	int pos = partition(arr, low, high, pivot);
+	
+	quickSort(arr, low, pos-1);
+	quickSort(arr, pos+1, high);
+	}
+}
+
+int main()
+{
+	int n ;
+	cout << " enter the size of array";
+	cin>>n;
+	int arr[n];
+	for( int i = 0 ; i < n; i++){
+		cin>> arr[i];
+	}
+	quickSort(arr, 0 , n-1);
+	cout<<"The sorted array is: ";
+	for( int i = 0 ; i < n; i++){
+		cout<< arr[i]<<"\t";
+	}
+	
+}
+ 
 void printArr(int a[], int n)
 {
     int i;
