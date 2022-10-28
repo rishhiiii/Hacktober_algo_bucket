a9cb7c25d4355f89fc63f451582f667741d0b46a
rishhiiii <87380598+rishhiiii@users.noreply.github.com>
2022-10-28 23:31:21 +0530

    Update NQueensProbleam.cpp

--- Recursion/NQueensProbleam.cpp
+++ Recursion/NQueensProbleam.cpp
@@ -1,77 +1,112 @@
+
+/* C++ program to solve N Queen Problem using
+backtracking */
+
 #include <bits/stdc++.h>
-#define endl "\n"
+#define N 4
 using namespace std;
 
-//AKASH DINKAR PATIL 
-//Q--> Input: n = 4
-//Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
-//Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
-
-//all formal parameters should be pass-by-referance
-void solve(int col,vector<string> &board, vector<int>&leftRow,vector<int>&lowerDiagonal,vector<int>&upperDiagonal, vector<vector<string>>&ans ,int n )
+/* A utility function to print solution */
+void printSolution(int board[N][N])
 {
-	if(col==n)
-	{
-		ans.push_back(board);
-		return;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++)
+			cout << " " << board[i][j] << " ";
+		printf("\n");
 	}
+}
 
-	for(int row=0;row<n;row++)
-	{
-		if(leftRow[row]==0 && upperDiagonal[n-1+col-row]==0 && lowerDiagonal[row+col]==0)
-		{
-			board[row][col]='Q';
-			leftRow[row]=1;
-			lowerDiagonal[row+col]=1;
-			upperDiagonal[n-1+col-row]=1;
-			solve(col+1,board,leftRow,lowerDiagonal,upperDiagonal,ans,n);  //recursive
-			board[row][col]='.';	
-			leftRow[row]=0;
-			lowerDiagonal[row+col]=0;
-			upperDiagonal[n-1+col-row]=0;
+/* A utility function to check if a queen can
+be placed on board[row][col]. Note that this
+function is called when "col" queens are
+already placed in columns from 0 to col -1.
+So we need to check only left side for
+attacking queens */
+bool isSafe(int board[N][N], int row, int col)
+{
+	int i, j;
+
+	/* Check this row on left side */
+	for (i = 0; i < col; i++)
+		if (board[row][i])
+			return false;
+
+	/* Check upper diagonal on left side */
+	for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+		if (board[i][j])
+			return false;
+
+	/* Check lower diagonal on left side */
+	for (i = row, j = col; j >= 0 && i < N; i++, j--)
+		if (board[i][j])
+			return false;
+
+	return true;
+}
+
+/* A recursive utility function to solve N
+Queen problem */
+bool solveNQUtil(int board[N][N], int col)
+{
+	/* base case: If all queens are placed
+	then return true */
+	if (col >= N)
+		return true;
+
+	/* Consider this column and try placing
+	this queen in all rows one by one */
+	for (int i = 0; i < N; i++) {
+		/* Check if the queen can be placed on
+		board[i][col] */
+		if (isSafe(board, i, col)) {
+			/* Place this queen in board[i][col] */
+			board[i][col] = 1;
+
+			/* recur to place rest of the queens */
+			if (solveNQUtil(board, col + 1))
+				return true;
+
+			/* If placing queen in board[i][col]
+			doesn't lead to a solution, then
+			remove queen from board[i][col] */
+			board[i][col] = 0; // BACKTRACK
 		}
 	}
+
+	/* If the queen cannot be placed in any row in
+		this column col then return false */
+	return false;
 }
 
-vector<vector<string>> solveNQueens(int n)
+/* This function solves the N Queen problem using
+Backtracking. It mainly uses solveNQUtil() to
+solve the problem. It returns false if queens
+cannot be placed, otherwise, return true and
+prints placement of queens in the form of 1s.
+Please note that there may be more than one
+solutions, this function prints one of the
+feasible solutions.*/
+bool solveNQ()
 {
-	vector<vector<string>>ans;
-	vector<string>board(n);
-	vector<int> leftRow(n,0),lowerDiagonal(2*n-1,0),upperDiagonal(2*n-1,0);
-	string s(n,'.');
-	//cout<<s<<endl;
-	for(int i=0;i<n;++i) board[i]=s;
+	int board[N][N] = { { 0, 0, 0, 0 },
+						{ 0, 0, 0, 0 },
+						{ 0, 0, 0, 0 },
+						{ 0, 0, 0, 0 } };
 
-	solve(0,board,leftRow,lowerDiagonal,upperDiagonal,ans,n); 
-	return ans;
+	if (solveNQUtil(board, 0) == false) {
+		cout << "Solution does not exist";
+		return false;
+	}
+
+	printSolution(board);
+	return true;
 }
 
-
+// driver program to test above function
 int main()
 {
-	/*#ifndef ONLINE_JUDGE
-	freopen("input.txt","r",stdin);            //code for automated testing
-	freopen("output.txt","w",stdout);
-	#endif */
-
-	int n;
-	cin>>n;
-	
-	vector<vector<string>>akash;
-	
-	akash=solveNQueens(n);
-
-	for(auto x :akash)
-	{
-		for(auto y:x)
-		{
-			cout<<y<<endl;
-		}
-		cout<<endl;
-	}
-	
-	
-	/* code */
+	solveNQ();
 	return 0;
 }
 
+// This code is contributed by Aditya Kumar (adityakumar129)
